1bafeebd2fdecd6227d9bc56b09042ddbaef8816
Cuong <kieucuong01@gmail.com>
2026-06-24 10:57:20 +0700

    Update June report for Thu Dau Mot snapshot

---
+++ tests/test_public_report_tdm_scope.py
@@ -0,0 +1,11 @@
+def test_binh_duong_market_report_page_now_focuses_on_thu_dau_mot_wards_only():
+    import app as radar_app
+
+    html = radar_app.app.test_client().get("/bao-cao/bds-binh-duong-thang-06-2026").get_data(as_text=True)
+
+    assert "chỉ tập trung các phường Thủ Dầu Một" in html
+    assert "Phú Mỹ" in html
+    assert "Định Hòa" in html
+    assert "Hiệp Thành" in html
+    assert "Hiệp An" in html
+    assert "Mỹ Phước" not in html
